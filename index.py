82309f42512eea6467d7ecada6562b4cb6059de3
REAGAN-INDUSTRIES <reaganindustriesedu@gmail.com>
2024-07-03 20:38:42 +0300

    Introduction

---
+++ index.py
@@ -0,0 +1,19 @@
+#Python can evaluate numeric expressions
+print(2+2)
+print(3-1)
+print(2*2)
+print(5/6)
+#double astorick is a power or exponential sign
+print(2**3)
+#If you want the result without the remainder
+print(5//2)
+#If you want only the remainder
+print(5%2)
+#It also follows the BODMAS rule
+print(2+8-7*5+(6/3))
+#you can either use double or single quotes
+print('REAGAN INDUSTRIES')
+print("Quantanamia")
+#When using an apostrophie, use double quotes
+print("How's it going pal?")
+
